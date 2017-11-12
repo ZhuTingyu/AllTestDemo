5661d5e2e51d1912b5c4ee40c4b555306ea90f84
ZhuTingyu <tingyu.zhu@biz-united.com.cn>
2017-11-12 23:12:52 +0800

    vedio

--- app/src/main/java/com/live/demo/adapter/VideoListAdapter.java
+++ app/src/main/java/com/live/demo/adapter/VideoListAdapter.java
@@ -1,11 +1,18 @@
 package com.live.demo.adapter;
 
 
+import android.net.Uri;
+import android.widget.FrameLayout;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
 import com.biz.base.BaseViewHolder;
 import com.biz.util.Lists;
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.live.demo.R;
 
+
+import org.opencv.video.Video;
 
 import cn.jzvd.JZVideoPlayerStandard;
 
@@ -20,10 +27,23 @@ public class VideoListAdapter extends BaseQuickAdapter<String, BaseViewHolder> {
     }
 
     @Override
-    protected void convert(BaseViewHolder helper, String item) {
-        JZVideoPlayerStandard jzVideoPlayerStandard = (JZVideoPlayerStandard) helper.getView(R.id.videoplayer);
+    protected void convert(BaseViewHolder holder, String item) {
+        /*JZVideoPlayerStandard jzVideoPlayerStandard = (JZVideoPlayerStandard) helper.getView(R.id.videoplayer);
         jzVideoPlayerStandard.setUp(item
-                , JZVideoPlayerStandard.SCREEN_STATE_ON, "饺子闭眼睛");
+                , JZVideoPlayerStandard.SCREEN_STATE_ON, "饺子闭眼睛");*/
+
+        /*FrameLayout fl = holder.getView(R.id.fl);
+        VideoView videoView = holder.getView(R.id.video_view);
+        videoView.setVideoURI(Uri.parse(item));
+        videoView.setMediaController(new MediaController(fl.getContext()));
+
+        holder.getView(R.id.play).setOnClickListener(v -> {
+            videoView.start();
+        });*/
+
+        /*McoyVideoView videoView = ((McoyVideoView) holder.getView(R.id.videoView));
+
+        videoView.setVideoUrl(item);*/
     }
 
 
